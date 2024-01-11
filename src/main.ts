cede8ab456f5c64a5c6057e3d20b85fcf7c3dced
Salazar34 <salazar34@outlook.it>
2024-01-11 11:48:45 +0100

    working on features

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,7 @@
 import './style.css';
 import 'flowbite';
+
+const f1 = document.getElementById('f1') as HTMLHeadingElement;
+const f2 = document.getElementById('f2') as HTMLHeadingElement;
+const f3 = document.getElementById('f3') as HTMLHeadingElement;
+const f4 = document.getElementById('f4') as HTMLHeadingElement;
